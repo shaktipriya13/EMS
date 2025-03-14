53758bf92a3a1160dc805374fbeb922f1c80daa6
Shakti Priya <priyashakti68@gmail.com>
2025-03-15 01:45:34 +0530

    Implement task creation feature

--- src/component/Dashboard/EmployeeDashboard.jsx
+++ src/component/Dashboard/EmployeeDashboard.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import Header from '../other/Header';
-import TaskListNumbers from '../other/TaskListNumbers';
-import TaskList from '../TaskList/TaskList';
+import React from 'react'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = ({data}) => {
-  // console.log(props);
+const EmployeeDashboard = (props) => {
+
   return (
-    <div className='p-10 bg-[#1C1C1C] h-screen'>  
-    
-      {/* <h1>{data.id}</h1> */}
-       <Header data={data}/>
-       <TaskListNumbers data={data}/>
-       <TaskList data={data}/>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+        
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
     </div>
-  );
+  )
 }
 
-export default EmployeeDashboard;
+export default EmployeeDashboard
